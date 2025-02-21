9c6acd89c0e87f7d3cb8991b412f37fdfbc0d6e9
Arunprasath P <arunprasath.p@microchip.com>
2025-02-21 18:39:57 +0530

    peripherals: dma: microchip: rename dma hal file
    Renamed HAL file hal_mchp_dma.h to hal_mchp_dma_dmac_u2503.h
    and updated the directory structure.
    
    Signed-off-by: Arunprasath P <arunprasath.p@microchip.com>

---
+++ peripherals/dmac/hal_mchp_dma_dmac_u2503.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file hal_mchp_dma_dmac_u2503.h
+ * @brief HAL interface for Microchip DMAC U2503.
+ */
+
+#ifndef MICROCHIP_HAL_MCHP_DMA_DMAC_U2503_H_
+#define MICROCHIP_HAL_MCHP_DMA_DMAC_U2503_H_
+
+/**
+ * @brief Enable the DMA controller.
+ *
+ * This function performs any necessary steps to enable the DMA controller.
+ */
+static inline void hal_mchp_dma_enable(void)
+{
+	/* Implementation for enabling the DMA controller */
+}
+
+#endif /* MICROCHIP_HAL_MCHP_DMA_DMAC_U2503_H_ */
--- peripherals/dmac_u2503/hal_mchp_dma.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) [2025] [Microchip Technology Inc.]
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-static inline void hal_mchp_dma_config_v1(void) {}
