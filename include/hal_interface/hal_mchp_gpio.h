f00388c81f2a15f6c55b552697df7082a7e7cc14
Mohamed Azhar <mohamed.azhar@microchip.com>
2025-04-07 10:55:18 +0530

    modules: hal_microchip: gpio: add hal functions
    add support for GPIO_DISCONNECTED and INPUT/OUTPUT feature
    
    Signed-off-by: Mohamed Azhar <mohamed.azhar@microchip.com>

--- include/hal_interface/hal_mchp_gpio.h
+++ include/hal_interface/hal_mchp_gpio.h
@@ -20,17 +20,17 @@
  * @def HAL_MCHP_GPIO_ERR_NOTSUP
  * @brief Macro indicating the feature is not supported.
  */
-#define HAL_MCHP_GPIO_ERR_NOTSUP           	-1
+#define HAL_MCHP_GPIO_ERR_NOTSUP -1
 /**
  * @def HAL_MCHP_GPIO_SUCCESS
  * @brief Macro indicating successful operation.
  */
-#define HAL_MCHP_GPIO_SUCCESS           	0
+#define HAL_MCHP_GPIO_SUCCESS    0
 /**
  * @def HAL_BIT(n)
  * @brief Unsigned integer with bit position 'n' set.
  */
-#define HAL_BIT(n)  						(1UL << (n))
+#define HAL_BIT(n)               (1UL << (n))
 
 /** GPIO HAL Function prototypes */
 
@@ -49,7 +49,7 @@ static inline void hal_mchp_gpio_port_get(hal_gpio_port_reg *regs, uint32_t *por
  * @param value Value to set on the port.
  */
 static inline void hal_mchp_gpio_port_set_masked(hal_gpio_port_reg *regs, uint32_t mask,
-                                                 uint32_t value);
+						 uint32_t value);
 /**
  * @brief Enable input on a specific GPIO pin.
  *
@@ -86,12 +86,23 @@ static inline void hal_mchp_gpio_set_dir_output(hal_gpio_port_reg *regs, const u
  */
 static inline void hal_mchp_gpio_set_dir_input(hal_gpio_port_reg *regs, const uint32_t pin);
 /**
+ * @brief Set GPIO pin direction to input/output.
+ *
+ * This function sets the direction of a specified GPIO pin to input output.
+ *
+ * @param regs Pointer to the GPIO port register structure.
+ * @param pin The pin number to set the direction for.
+ * @retval 0 If successful
+ * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feature is not supported.
+ */
+static inline int hal_mchp_gpio_set_dir_input_output(hal_gpio_port_reg *regs, const uint32_t pin);
+/**
  * @brief Enable pull-up/pull-down resistor on a specific GPIO pin.
  *
  * @param regs Pointer to the port group registers.
  * @param pin Pin number to enable pull resistor on.
  */
-static inline void hal_mchp_gpio_enable_pullup(hal_gpio_port_reg *regs, const uint32_t pin);
+static inline int hal_mchp_gpio_enable_pullup(hal_gpio_port_reg *regs, const uint32_t pin);
 /**
  * @brief Check if pull-up/pull-down resistor is enabled on a specific GPIO pin.
  *
@@ -106,24 +117,21 @@ static inline bool hal_mchp_gpio_is_pullup(hal_gpio_port_reg *regs, const uint32
  * @param regs Pointer to the port group registers.
  * @param pins Bitmask of pins to set high.
  */
-static inline void hal_mchp_gpio_set_pins_high(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins);
+static inline void hal_mchp_gpio_set_pins_high(hal_gpio_port_reg *regs, const uint32_t pins);
 /**
  * @brief Set multiple GPIO pins to low.
  *
  * @param regs Pointer to the port group registers.
  * @param pins Bitmask of pins to clear low.
  */
-static inline void hal_mchp_gpio_set_pins_low(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins);
+static inline void hal_mchp_gpio_set_pins_low(hal_gpio_port_reg *regs, const uint32_t pins);
 /**
  * @brief Toggle multiple GPIO pins.
  *
  * @param regs Pointer to the port group registers.
  * @param pins Bitmask of pins to toggle.
  */
-static inline void hal_mchp_gpio_toggle_pins(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins);
+static inline void hal_mchp_gpio_toggle_pins(hal_gpio_port_reg *regs, const uint32_t pins);
 /**
  * @brief Check if a specific GPIO pin is set to high.
  *
@@ -132,14 +140,13 @@ static inline void hal_mchp_gpio_toggle_pins(hal_gpio_port_reg *regs,
  * @retval true if the GPIO pin is high.
  * @retval false if the GPIO pin is not high.
  */
-static inline bool hal_mchp_gpio_is_pin_high(hal_gpio_port_reg *regs,
-                                                    const uint32_t pin);
+static inline bool hal_mchp_gpio_is_pin_high(hal_gpio_port_reg *regs, const uint32_t pin);
 /**
  * @brief Get the direction configuration of the GPIO port.
  *
  * @param regs Pointer to the port group registers.
  * @return Direction configuration of the port.
- *         The uint32_t return value will be a pin mask, 
+ *         The uint32_t return value will be a pin mask,
  *         where pin configured as Output is represented as '1' and Input as '0'
  */
 static inline uint32_t hal_mchp_gpio_port_get_dir(hal_gpio_port_reg *regs);
@@ -149,16 +156,34 @@ static inline uint32_t hal_mchp_gpio_port_get_dir(hal_gpio_port_reg *regs);
  * @param regs Pointer to the port group registers.
  * @param pin Pin number to check.
  * @retval true if the GPIO pin is configured as output.
- * @retval false if the GPIO pin is not configured as output. 
+ * @retval false if the GPIO pin is not configured as output.
  */
 static inline bool hal_mchp_gpio_is_pin_output(hal_gpio_port_reg *regs, const uint32_t pin);
+/**
+ * @brief Get the input pins of a GPIO port.
+ *
+ * This function retrieves the current input pin states of the specified GPIO port.
+ *
+ * @param regs Pointer to the GPIO port register structure.
+ * @return uint32_t Bitmask representing the input pin states of the GPIO port.
+ */
+static inline uint32_t hal_mchp_gpio_port_get_input_pins(hal_gpio_port_reg *regs);
+/**
+ * @brief Get the output pins of a GPIO port.
+ *
+ * This function retrieves the current output pin states of the specified GPIO port.
+ *
+ * @param regs Pointer to the GPIO port register structure.
+ * @return uint32_t Bitmask representing the output pin states of the GPIO port.
+ */
+static inline uint32_t hal_mchp_gpio_port_get_output_pins(hal_gpio_port_reg *regs);
 /**
  * @brief Set the GPIO pins to open-drain mode.
  *
  * @param regs Pointer to the port group registers.
  * @param pins Bitmask of pins to set to open-drain.
  * @retval 0 If successful
- * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feture is not supported. 
+ * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feature is not supported.
  */
 static inline int hal_mchp_gpio_set_open_drain(hal_gpio_port_reg *regs, const uint32_t pins);
 /**
@@ -167,7 +192,7 @@ static inline int hal_mchp_gpio_set_open_drain(hal_gpio_port_reg *regs, const ui
  * @param regs Pointer to the port group registers.
  * @param pins Bitmask of pins to set to open-source.
  * @retval 0 If successful
- * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feture is not supported. 
+ * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feature is not supported.
  */
 static inline int hal_mchp_gpio_set_open_source(hal_gpio_port_reg *regs, const uint32_t pins);
 /**
@@ -176,7 +201,19 @@ static inline int hal_mchp_gpio_set_open_source(hal_gpio_port_reg *regs, const u
  * @param regs Pointer to the port group registers.
  * @param pin Pin number to check.
  * @retval true if the GPIO pin is configured as open-drain.
- * @retval false if the GPIO pin is not configured as open-drain. 
+ * @retval false if the GPIO pin is not configured as open-drain.
  */
- static inline bool hal_mchp_gpio_is_pin_open_drain(hal_gpio_port_reg *regs, const uint32_t pin);
+static inline bool hal_mchp_gpio_is_pin_open_drain(hal_gpio_port_reg *regs, const uint32_t pin);
+/**
+ * @brief Disconnects a GPIO pin.
+ *
+ * This function disconnects the specified GPIO pin by modifying the
+ * corresponding registers.
+ *
+ * @param regs Pointer to the GPIO port registers.
+ * @param pin The pin number to be disconnected.
+ * @retval 0 If successful
+ * @retval HAL_MCHP_GPIO_ERR_NOTSUP if the feature is not supported
+ */
+static inline int hal_mchp_gpio_disconnect(hal_gpio_port_reg *regs, const uint32_t pin);
 #endif /* MICROCHIP_HAL_MCHP_GPIO_H_ */
--- peripherals/port/hal_mchp_gpio_port_u2210.h
+++ peripherals/port/hal_mchp_gpio_port_u2210.h
@@ -15,6 +15,7 @@
 #ifndef MICROCHIP_HAL_MCHP_GPIO_PORT_U2210_H_
 #define MICROCHIP_HAL_MCHP_GPIO_PORT_U2210_H_
 
+#include <stdint.h>
 #include <hal_interface/hal_mchp_gpio.h>
 
 /**
@@ -22,16 +23,16 @@
  */
 static inline void hal_mchp_gpio_port_get(hal_gpio_port_reg *regs, uint32_t *port_val)
 {
-    *port_val = regs->PORT_IN;
+	*port_val = regs->PORT_IN;
 }
 
 /**
  * Set the GPIO port output value with a mask.
  */
 static inline void hal_mchp_gpio_port_set_masked(hal_gpio_port_reg *regs, uint32_t mask,
-                                                 uint32_t value)
+						 uint32_t value)
 {
-    regs->PORT_OUT = (regs->PORT_OUT & ~(mask)) | (value & mask);
+	regs->PORT_OUT = (regs->PORT_OUT & ~(mask)) | (value & mask);
 }
 
 /**
@@ -39,7 +40,7 @@ static inline void hal_mchp_gpio_port_set_masked(hal_gpio_port_reg *regs, uint32
  */
 static inline void hal_mchp_gpio_enable_input(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-    regs->PORT_PINCFG[pin] |= PORT_PINCFG_INEN(1);
+	regs->PORT_PINCFG[pin] |= PORT_PINCFG_INEN(1);
 }
 
 /**
@@ -47,7 +48,7 @@ static inline void hal_mchp_gpio_enable_input(hal_gpio_port_reg *regs, const uin
  */
 static inline void hal_mchp_gpio_outset(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-    regs->PORT_OUTSET |= HAL_BIT(pin);
+	regs->PORT_OUTSET = HAL_BIT(pin);
 }
 
 /**
@@ -55,7 +56,7 @@ static inline void hal_mchp_gpio_outset(hal_gpio_port_reg *regs, const uint32_t 
  */
 static inline void hal_mchp_gpio_outclr(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-    regs->PORT_OUTCLR |= HAL_BIT(pin);
+	regs->PORT_OUTCLR = HAL_BIT(pin);
 }
 
 /**
@@ -63,7 +64,8 @@ static inline void hal_mchp_gpio_outclr(hal_gpio_port_reg *regs, const uint32_t 
  */
 static inline void hal_mchp_gpio_set_dir_output(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-    regs->PORT_DIRSET |= HAL_BIT(pin);
+	regs->PORT_PINCFG[pin] &= ~PORT_PINCFG_INEN(1);
+	regs->PORT_DIRSET = HAL_BIT(pin);
 }
 
 /**
@@ -72,15 +74,26 @@ static inline void hal_mchp_gpio_set_dir_output(hal_gpio_port_reg *regs, const u
 static inline void hal_mchp_gpio_set_dir_input(hal_gpio_port_reg *regs, const uint32_t pin)
 {
 	hal_mchp_gpio_enable_input(regs, pin);
-	regs->PORT_DIRCLR |= HAL_BIT(pin);
+	regs->PORT_DIRCLR = HAL_BIT(pin);
+}
+
+/**
+ * Set the direction of a specific GPIO pin to input output.
+ */
+static inline int hal_mchp_gpio_set_dir_input_output(hal_gpio_port_reg *regs, const uint32_t pin)
+{
+	hal_mchp_gpio_enable_input(regs, pin);
+	regs->PORT_DIRSET = HAL_BIT(pin);
+	return 0;
 }
 
 /**
  * Enable pull-up/pull-down resistor on a specific GPIO pin.
  */
-static inline void hal_mchp_gpio_enable_pullup(hal_gpio_port_reg *regs, const uint32_t pin)
+static inline int hal_mchp_gpio_enable_pullup(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-    regs->PORT_PINCFG[pin] |= PORT_PINCFG_PULLEN(1);
+	regs->PORT_PINCFG[pin] |= PORT_PINCFG_PULLEN(1);
+	return 0;
 }
 
 /**
@@ -88,51 +101,47 @@ static inline void hal_mchp_gpio_enable_pullup(hal_gpio_port_reg *regs, const ui
  */
 static inline bool hal_mchp_gpio_is_pullup(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-	bool is_pull_enabled =  false;
-    if((regs->PORT_PINCFG[pin] & PORT_PINCFG_PULLEN(1)) != 0){
+	bool is_pull_enabled = false;
+	if ((regs->PORT_PINCFG[pin] & PORT_PINCFG_PULLEN(1)) != 0) {
 		is_pull_enabled = true;
 	}
-	return is_pull_enabled;		
+	return is_pull_enabled;
 }
 
 /**
  * Set multiple GPIO pins to high.
  */
-static inline void hal_mchp_gpio_set_pins_high(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins)
+static inline void hal_mchp_gpio_set_pins_high(hal_gpio_port_reg *regs, const uint32_t pins)
 {
-    regs->PORT_OUTSET = pins;
+	regs->PORT_OUTSET = pins;
 }
 
 /**
  * Set multiple GPIO pins to low.
  */
-static inline void hal_mchp_gpio_set_pins_low(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins)
+static inline void hal_mchp_gpio_set_pins_low(hal_gpio_port_reg *regs, const uint32_t pins)
 {
-    regs->PORT_OUTCLR = pins;
+	regs->PORT_OUTCLR = pins;
 }
 
 /**
  * Toggle multiple GPIO pins.
  */
-static inline void hal_mchp_gpio_toggle_pins(hal_gpio_port_reg *regs,
-                                                   const uint32_t pins)
+static inline void hal_mchp_gpio_toggle_pins(hal_gpio_port_reg *regs, const uint32_t pins)
 {
-    regs->PORT_OUTTGL = pins;
+	regs->PORT_OUTTGL = pins;
 }
 
 /**
  * Check if a specific GPIO pin is set to high.
  */
-static inline bool hal_mchp_gpio_is_pin_high(hal_gpio_port_reg *regs,
-                                                    const uint32_t pin)
+static inline bool hal_mchp_gpio_is_pin_high(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-	bool is_output_high =  false;
-    if((regs->PORT_OUT & HAL_BIT(pin)) != 0){
+	bool is_output_high = false;
+	if ((regs->PORT_OUT & HAL_BIT(pin)) != 0) {
 		is_output_high = true;
 	}
-	return is_output_high;	
+	return is_output_high;
 }
 
 /**
@@ -140,7 +149,7 @@ static inline bool hal_mchp_gpio_is_pin_high(hal_gpio_port_reg *regs,
  */
 static inline uint32_t hal_mchp_gpio_port_get_dir(hal_gpio_port_reg *regs)
 {
-    return regs->PORT_DIR;
+	return regs->PORT_DIR;
 }
 
 /**
@@ -148,11 +157,37 @@ static inline uint32_t hal_mchp_gpio_port_get_dir(hal_gpio_port_reg *regs)
  */
 static inline bool hal_mchp_gpio_is_pin_output(hal_gpio_port_reg *regs, const uint32_t pin)
 {
-	bool is_output =  false;
-    if((hal_mchp_gpio_port_get_dir(regs) & HAL_BIT(pin)) != 0){
+	bool is_output = false;
+	if ((hal_mchp_gpio_port_get_dir(regs) & HAL_BIT(pin)) != 0) {
 		is_output = true;
 	}
 	return is_output;
+}
+
+/**
+ * Get the pins configured as input.
+ */
+static inline uint32_t hal_mchp_gpio_port_get_input_pins(hal_gpio_port_reg *regs)
+{
+	uint32_t pin_id;
+	uint32_t port_input = 0;
+	for (pin_id = 0; pin_id < 32; pin_id++) {
+		if (((regs->PORT_PINCFG[pin_id] & PORT_PINCFG_INEN(1)) != 0)) {
+			port_input |= HAL_BIT(pin_id);
+		}
+	}
+
+	return port_input;
+}
+
+/**
+ * Get the pins configured as output.
+ */
+static inline uint32_t hal_mchp_gpio_port_get_output_pins(hal_gpio_port_reg *regs)
+{
+	uint32_t port_output = 0;
+	port_output = hal_mchp_gpio_port_get_dir(regs);
+	return port_output;
 }
 
 /**
@@ -160,8 +195,8 @@ static inline bool hal_mchp_gpio_is_pin_output(hal_gpio_port_reg *regs, const ui
  */
 static inline int hal_mchp_gpio_set_open_drain(hal_gpio_port_reg *regs, const uint32_t pins)
 {
-    /* Not Supported */
-    return HAL_MCHP_GPIO_ERR_NOTSUP;
+	/* Not Supported */
+	return HAL_MCHP_GPIO_ERR_NOTSUP;
 }
 
 /**
@@ -169,16 +204,29 @@ static inline int hal_mchp_gpio_set_open_drain(hal_gpio_port_reg *regs, const ui
  */
 static inline int hal_mchp_gpio_set_open_source(hal_gpio_port_reg *regs, const uint32_t pins)
 {
-    /* Not Supported */
-    return HAL_MCHP_GPIO_ERR_NOTSUP;
+	/* Not Supported */
+	return HAL_MCHP_GPIO_ERR_NOTSUP;
 }
 
 /**
  * Check if the GPIO pin is set to open-source mode.
  */
- static inline bool hal_mchp_gpio_is_pin_open_drain(hal_gpio_port_reg *regs, const uint32_t pin)
- {
-    /* Not Supported */
-    return false;
- }
+static inline bool hal_mchp_gpio_is_pin_open_drain(hal_gpio_port_reg *regs, const uint32_t pin)
+{
+	/* Not Supported */
+	return false;
+}
+
+/**
+ * Disconnect the GPIO pin.
+ */
+static inline int hal_mchp_gpio_disconnect(hal_gpio_port_reg *regs, const uint32_t pin)
+{
+	/* Disable pull-up/pull-down resistor on a specific GPIO pin. */
+	regs->PORT_PINCFG[pin] &= ~PORT_PINCFG_PULLEN(1);
+	/* Disable input on a specific GPIO pin. */
+	regs->PORT_PINCFG[pin] &= ~PORT_PINCFG_INEN(1);
+	regs->PORT_DIRCLR = HAL_BIT(pin);
+	return 0;
+}
 #endif /* MICROCHIP_HAL_MCHP_GPIO_PORT_U2210_H_ */
